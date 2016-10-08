83414642719e33bd51bf83c5c7d7532658e3c45a
xv330790416 <5287xv123456>
2016-10-08 13:41:40 +0800

    第一次提交

---
+++ src/main/java/com/wasu/control/FirstController.java
@@ -0,0 +1,10 @@
+package com.wasu.control;
+
+/**
+ * Created by kale on 2016/10/8.
+ */
+public class FirstController {
+    public void main(String[] args){
+        System.out.println("helloworld!");
+    }
+}
